49528ecd5728afabb5dd35e2cddb69f8e523f87a
mnemoli <prismmirrorlens@gmail.com>
2023-04-05 12:43:56 +0100

    Fix using old setting name

--- scene/main/scene_tree.cpp
+++ scene/main/scene_tree.cpp
@@ -1523,7 +1523,7 @@ SceneTree::SceneTree() {
 #endif // _3D_DISABLED
 
 	root->set_physics_object_picking(GLOBAL_DEF("physics/common/enable_object_picking", true));
-	root->set_physics_object_picking_top_only(GLOBAL_DEF("physics/common/enable_physics_object_picking_top_only", false));
+	root->set_physics_object_picking_top_only(GLOBAL_DEF("physics/common/enable_object_picking_top_only", false));
 
 	root->connect("close_requested", callable_mp(this, &SceneTree::_main_window_close));
 	root->connect("go_back_requested", callable_mp(this, &SceneTree::_main_window_go_back));
